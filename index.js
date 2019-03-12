6c6da23e3a99913df59d2a9e72d8a0df133dc40e
Nathan Miranda <nate845@gmail.com>
2019-03-12 00:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const headquartersBlock = 42
 
 // Code your solution in this file!
 function distanceFromHqInBlocks(block) {
-  return block - headquartersBlock
+  return Math.block - headquartersBlock
 }
