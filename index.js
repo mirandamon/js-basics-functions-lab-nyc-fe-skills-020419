2cff5828aa24b4183f82564ff18d8cbfa683f96a
Nathan Miranda <nate845@gmail.com>
2019-03-12 00:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const headquartersBlock = 42
+const feetPerBlock = 264
 
 // Code your solution in this file!
 function distanceFromHqInBlocks(block) {
   return Math.abs(block - headquartersBlock)
 }
 
-function distanceFromHqInFeet() {
-  
+function distanceFromHqInFeet(block) {
+  // return Math.abs(block - headquartersBlock) * feetPerBlock
+  return distanceFromHqInBlocks(block) * feetPerBlock
 }
